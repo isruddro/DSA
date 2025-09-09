0eee6d3a064c1f7b4299eca8f1369f0a4b04d9e9
Protap Ruddrodeb Sarker <93969345+isruddro@users.noreply.github.com>
2025-09-10 00:31:05 +0600

    Update 12_sequence_patttern_matching.cs

--- Aditya Verma/Dynamic Programming/03 Longest Common Subsequence (LCS)/12_sequence_patttern_matching.cs
+++ Aditya Verma/Dynamic Programming/03 Longest Common Subsequence (LCS)/12_sequence_patttern_matching.cs
@@ -1,3 +1,32 @@
+py:
+
+def LCS(X, Y):
+    n = len(X)
+    m = len(Y)
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
+
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if X[i - 1] == Y[j - 1]:
+                dp[i][j] = 1 + dp[i - 1][j - 1]
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+    return dp[n][m]
+
+def SeqPatternMatching(X, Y):
+    return LCS(X, Y) == min(len(X), len(Y))
+
+# Example usage
+X = input()
+Y = input()
+
+print("YES" if SeqPatternMatching(X, Y) else "NO")
+
+
+        
+
+cpp:
+
 #include <bits/stdc++.h>
 using namespace std;
 
