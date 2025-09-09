782e94ce3ff32b211427abdb66f0610bdd337db5
Protap Ruddrodeb Sarker <93969345+ruddro-dev@users.noreply.github.com>
2025-09-09 10:29:06 +0600

    Update 010 Permute a string by changing case-GFG.cs

--- Aditya Verma/Recursion/010 Permute a string by changing case-GFG.cs
+++ Aditya Verma/Recursion/010 Permute a string by changing case-GFG.cs
@@ -1,3 +1,46 @@
+cpp:
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void GetSolve(string op, string s, vector<string>& result) {
+        if (s.empty()) {
+            result.push_back(op);
+            return;
+        }
+
+        string op1 = op;
+        string op2 = op;
+
+        op1 += tolower(s[0]);
+        op2 += toupper(s[0]);
+
+        s = s.substr(1); // Remove the first character
+        GetSolve(op1, s, result);
+        GetSolve(op2, s, result);
+    }
+};
+
+int main() {
+    string s;
+    cin >> s; // Input string
+
+    vector<string> result;
+    string op = "";
+
+    Solution solution;
+    solution.GetSolve(op, s, result);
+
+    for (auto& res : result) {
+        cout << res << " ";
+    }
+
+    return 0;
+}
+
+
+c#:
 using System;
 using System.Collections.Generic;
 
