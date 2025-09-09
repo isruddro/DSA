5584ab250d68f786671de60e1bb62b2b352d3af7
Protap Ruddrodeb Sarker <93969345+ruddro-dev@users.noreply.github.com>
2025-09-09 10:20:37 +0600

    Update FreqSort.cs

--- Aditya Verma/Heaps/FreqSort.cs
+++ Aditya Verma/Heaps/FreqSort.cs
@@ -1,3 +1,44 @@
+cpp:
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Initialize the array
+    vector<int> arr = {4, 4, 4, 4, 3, 3, 1, 1, 1, 2};
+    int n = arr.size();
+
+    // Map to store the frequency of each element
+    unordered_map<int, int> freq;
+    for (int num : arr) {
+        freq[num]++;
+    }
+
+    // Max-heap (priority queue) to store pairs of (frequency, element)
+    priority_queue<pair<int, int>> maxh;
+
+    for (auto &entry : freq) {
+        // Push (frequency, element) pair; priority_queue sorts by first element of pair by default
+        maxh.push({entry.second, entry.first});
+    }
+
+    // Print elements in frequency sorted order
+    while (!maxh.empty()) {
+        int f = maxh.top().first;
+        int ele = maxh.top().second;
+        maxh.pop();
+
+        for (int i = 0; i < f; i++) {
+            cout << ele << " ";
+        }
+    }
+    cout << endl;
+
+    return 0;
+}
+
+
+c#:
+
 using System;
 using System.Collections.Generic;
 
