cfcf33955d173bcb9f19372f24f6d33bfaa7d430
Protap Ruddrodeb Sarker <93969345+ruddro-dev@users.noreply.github.com>
2025-09-09 10:30:10 +0600

    Update 012 22. Generate Parentheses.cs

--- Aditya Verma/Recursion/012 22. Generate Parentheses.cs
+++ Aditya Verma/Recursion/012 22. Generate Parentheses.cs
@@ -1,3 +1,50 @@
+cpp:
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+private:
+    void GetSolved(string opt, int op, int cl, vector<string>& result) {
+        // If no more open and close parentheses to add, add the current combination to the result
+        if (op == 0 && cl == 0) {
+            result.push_back(opt);
+            return;
+        }
+
+        // If there are open parentheses remaining, add an open parenthesis
+        if (op > 0) {
+            GetSolved(opt + "(", op - 1, cl, result);
+        }
+
+        // If there are more closing parentheses than open ones, add a closing parenthesis
+        if (cl > op) {
+            GetSolved(opt + ")", op, cl - 1, result);
+        }
+    }
+
+public:
+    vector<string> GenerateParenthesis(int n) {
+        vector<string> result;
+        GetSolved("", n, n, result);
+        return result;
+    }
+};
+
+int main() {
+    int n;
+    cin >> n;
+
+    Solution solution;
+    vector<string> result = solution.GenerateParenthesis(n);
+
+    for (auto &s : result) {
+        cout << s << " ";
+    }
+    return 0;
+}
+
+
+c#:
 using System;
 using System.Collections.Generic;
 
