1cb373aecb09f9875f10d4e075c59c661221b25d
Protap Ruddrodeb Sarker <93969345+ruddro-dev@users.noreply.github.com>
2025-09-09 09:46:20 +0600

    Update 10 Next Alphabetical Element.cs

--- Aditya Verma/Binary Search/10 Next Alphabetical Element.cs
+++ Aditya Verma/Binary Search/10 Next Alphabetical Element.cs
@@ -1,3 +1,42 @@
+cpp:
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+char NextGreatestLetter(const vector<char>& letters, char target) {
+    int start = 0, end = letters.size() - 1;
+    int ans = 0; // Default to 0, handles wrap-around case if no greater letter is found.
+
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
+
+        if (letters[mid] <= target) {
+            start = mid + 1;
+        } else {
+            if (ans == 0 || letters[mid] < letters[ans])
+                ans = mid;
+            end = mid - 1;
+        }
+    }
+
+    return letters[ans];
+}
+
+int main() {
+    vector<char> letters = {'c', 'f', 'j'};
+    char target = 'a';
+
+    char result = NextGreatestLetter(letters, target);
+    cout << "The next greatest letter after " << target << " is " << result << endl;
+
+    return 0;
+}
+
+
+
+c#:
+
 using System;
 
 class Program
