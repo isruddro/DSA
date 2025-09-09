704b318f8541b116151169fd80d301c26479c03a
Protap Ruddrodeb Sarker <93969345+ruddro-dev@users.noreply.github.com>
2025-09-09 10:19:27 +0600

    Update 03_max_path_sum_from_leaf_to_leaf.cs

--- Aditya Verma/Dynamic Programming/05 DP on Trees/03_max_path_sum_from_leaf_to_leaf.cs
+++ Aditya Verma/Dynamic Programming/05 DP on Trees/03_max_path_sum_from_leaf_to_leaf.cs
@@ -1,5 +1,71 @@
 //Leaf to Leaf
 //so we don't consider the root, unless only one element is root. then we take that root.
+cpp:
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val) : data(val), left(nullptr), right(nullptr) {}
+};
+
+class Solution {
+private:
+    int res;
+
+    // Recursive function to calculate maximum leaf-to-leaf path sum
+    int Solve(Node* root) {
+        if (!root) return 0;
+
+        int left = Solve(root->left);
+        int right = Solve(root->right);
+
+        int temp;
+
+        // If both children exist, consider path through this node
+        if (root->left && root->right) {
+            res = max(res, left + right + root->data);  // Update global maximum
+            temp = root->data + max(left, right);      // Return max path sum starting from this node
+        }
+        else if (root->left) {
+            temp = root->data + left;
+        }
+        else if (root->right) {
+            temp = root->data + right;
+        }
+        else { // Leaf node
+            temp = root->data;
+        }
+
+        return temp;
+    }
+
+public:
+    int MaxPathSum(Node* root) {
+        res = INT_MIN;  // Initialize result to smallest value
+        Solve(root);
+        return res;
+    }
+};
+
+int main() {
+    // Example usage
+    Node* root = new Node(-10);
+    root->left = new Node(9);
+    root->right = new Node(20);
+    root->right->left = new Node(15);
+    root->right->right = new Node(7);
+
+    Solution sol;
+    cout << "Maximum Leaf-to-Leaf Path Sum: " << sol.MaxPathSum(root) << endl;
+
+    return 0;
+}
+
+
+c#:
 using System;
 
 public class Node {
