71b4e1051a7db2c665bde7e5b4bd61dd2c554946
Protap Ruddrodeb Sarker <93969345+ruddro-dev@users.noreply.github.com>
2025-09-09 09:45:38 +0600

    Update 09 Ceil in a sorted array.cs

--- Aditya Verma/Binary Search/09 Ceil in a sorted array.cs
+++ Aditya Verma/Binary Search/09 Ceil in a sorted array.cs
@@ -1,3 +1,46 @@
+cpp:
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int SearchInsert(const vector<int>& nums, int target) {
+    int start = 0, end = nums.size() - 1;
+    int ans = nums.size(); // If target is greater than all elements, insert at the end.
+
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
+
+        if (nums[mid] == target)
+            return mid;
+
+        if (nums[mid] < target) {
+            start = mid + 1;
+        } else {
+            if (ans == nums.size() || nums[mid] < nums[ans])
+                ans = mid;
+            end = mid - 1;
+        }
+    }
+
+    return ans;
+}
+
+int main() {
+    vector<int> nums = {1, 3, 5, 6};
+    int target = 5;
+
+    int result = SearchInsert(nums, target);
+    cout << "Insert position for " << target << " is " << result << endl;
+
+    return 0;
+}
+
+
+
+c#:
+
+
 using System;
 
 class Program
