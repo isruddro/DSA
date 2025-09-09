63f3d8994a4b2f5305d9d22a787756e5523bcd75
Protap Ruddrodeb Sarker <93969345+ruddro-dev@users.noreply.github.com>
2025-09-09 10:04:10 +0600

    Update 06_SCS.cs

--- Aditya Verma/Dynamic Programming/03 Longest Common Subsequence (LCS)/06_SCS.cs
+++ Aditya Verma/Dynamic Programming/03 Longest Common Subsequence (LCS)/06_SCS.cs
@@ -1,3 +1,49 @@
+cpp:
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+// Function to calculate LCS length
+int LCS(const string &X, const string &Y, int n, int m) {
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            if (X[i - 1] == Y[j - 1])
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            else
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+
+    return dp[n][m];
+}
+
+// Function to calculate SCS length
+int SCSLength(const string &X, const string &Y) {
+    int n = X.length();
+    int m = Y.length();
+    return n + m - LCS(X, Y, n, m);
+}
+
+int main() {
+    string X, Y;
+    getline(cin, X);
+    getline(cin, Y);
+
+    cout << SCSLength(X, Y) << endl;
+
+    return 0;
+}
+
+
+
+
+
+
+c#:
+
 using System;
 
 public class Solution
