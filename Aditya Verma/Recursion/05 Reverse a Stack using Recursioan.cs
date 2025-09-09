7c68709bb7ba3715278a035586d1edf02399d5ba
Protap Ruddrodeb Sarker <93969345+ruddro-dev@users.noreply.github.com>
2025-09-09 10:32:55 +0600

    Update 05 Reverse a Stack using Recursioan.cs

--- Aditya Verma/Recursion/05 Reverse a Stack using Recursioan.cs
+++ Aditya Verma/Recursion/05 Reverse a Stack using Recursioan.cs
@@ -1,3 +1,60 @@
+cpp:
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    // Helper function to insert element at the bottom of the stack
+    void getSolve(stack<int>& st, int t) {
+        if (st.empty()) {
+            st.push(t);
+            return;
+        }
+
+        int to = st.top();
+        st.pop();
+        getSolve(st, t);
+        st.push(to);
+    }
+
+    // Function to reverse the stack recursively
+    void solve(stack<int>& st) {
+        if (st.empty())
+            return;
+
+        int t = st.top();
+        st.pop();
+        solve(st);
+        getSolve(st, t);
+    }
+};
+
+int main() {
+    int n;
+    cin >> n;
+
+    stack<int> st;
+    for (int i = 0; i < n; i++) {
+        int val;
+        cin >> val;
+        st.push(val);
+    }
+
+    Solution solution;
+    solution.solve(st);
+
+    // Print stack elements from top to bottom
+    while (!st.empty()) {
+        cout << st.top() << " ";
+        st.pop();
+    }
+    cout << endl;
+
+    return 0;
+}
+
+
+c#:
 using System;
 using System.Collections.Generic;
 
