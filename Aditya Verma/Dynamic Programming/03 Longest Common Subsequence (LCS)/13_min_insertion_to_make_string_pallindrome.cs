93028c2726d2c4d8a1243c007b4e49c59b18e20d
Protap Ruddrodeb Sarker <93969345+ruddro-dev@users.noreply.github.com>
2025-09-09 10:10:00 +0600

    Update 13_min_insertion_to_make_string_pallindrome.cs

--- Aditya Verma/Dynamic Programming/03 Longest Common Subsequence (LCS)/13_min_insertion_to_make_string_pallindrome.cs
+++ Aditya Verma/Dynamic Programming/03 Longest Common Subsequence (LCS)/13_min_insertion_to_make_string_pallindrome.cs
@@ -1,3 +1,50 @@
+cpp:
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to compute the length of the Longest Common Subsequence (LCS)
+int LCS(const string &X, const string &Y) {
+    int n = X.length();
+    int m = Y.length();
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            if (X[i - 1] == Y[j - 1])
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            else
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+
+    return dp[n][m];
+}
+
+// Function to compute the Longest Palindromic Subsequence (LPS)
+int LPS(const string &X) {
+    string rev_X = X;
+    reverse(rev_X.begin(), rev_X.end());
+    return LCS(X, rev_X);
+}
+
+// Function to compute the minimum insertions required to make the string a palindrome
+int MinInsertForPalindrome(const string &X) {
+    return X.length() - LPS(X);
+}
+
+int main() {
+    string X;
+    cin >> X;
+
+    cout << MinInsertForPalindrome(X) << endl;
+
+    return 0;
+}
+
+
+
+c#:
+
 using System;
 
 class Program
