1962cc172e1ce2f4686880e50d26329bd4ad68e1
Protap Ruddrodeb Sarker <93969345+isruddro@users.noreply.github.com>
2025-09-10 00:24:45 +0600

    Update 04_LCSubstring.cs

--- Aditya Verma/Dynamic Programming/03 Longest Common Subsequence (LCS)/04_LCSubstring.cs
+++ Aditya Verma/Dynamic Programming/03 Longest Common Subsequence (LCS)/04_LCSubstring.cs
@@ -1,3 +1,33 @@
+py:
+
+def lc_substr(X, Y):
+    n = len(X)
+    m = len(Y)
+    
+    # Create DP table
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
+    mx = 0  # maximum length of common substring
+
+    for i in range(n + 1):
+        for j in range(m + 1):
+            if i == 0 or j == 0:
+                dp[i][j] = 0
+            elif X[i - 1] == Y[j - 1]:
+                dp[i][j] = 1 + dp[i - 1][j - 1]
+            else:
+                dp[i][j] = 0
+            mx = max(mx, dp[i][j])
+    
+    return mx
+
+# Example usage
+X = input()
+Y = input()
+result = lc_substr(X, Y)
+print(result)
+
+
+
 cpp:
 
 #include <iostream>
