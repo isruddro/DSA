8bd290416c812a9bcdbc07ec84863cf4af2f3344
Protap Ruddrodeb Sarker <93969345+ruddro-dev@users.noreply.github.com>
2025-09-09 09:41:12 +0600

    Update 03 First and Last Occ.cs

--- Aditya Verma/Binary Search/03 First and Last Occ.cs
+++ Aditya Verma/Binary Search/03 First and Last Occ.cs
@@ -1,3 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+int minIndex = INT_MAX;
+int maxIndex = INT_MIN;
+
+void BinarySearch(int start, int end, const vector<int>& nums, int target, bool leftBias) {
+    if (start > end) return;
+
+    int mid = (start + end) / 2;
+
+    if (nums[mid] > target) {
+        BinarySearch(start, mid - 1, nums, target, leftBias);
+    } 
+    else if (nums[mid] < target) {
+        BinarySearch(mid + 1, end, nums, target, leftBias);
+    } 
+    else {
+        if (leftBias) {
+            minIndex = min(minIndex, mid);
+            BinarySearch(start, mid - 1, nums, target, leftBias);
+        } else {
+            maxIndex = max(maxIndex, mid);
+            BinarySearch(mid + 1, end, nums, target, leftBias);
+        }
+    }
+}
+
+vector<int> SearchRange(const vector<int>& nums, int target) {
+    minIndex = INT_MAX;
+    maxIndex = INT_MIN;
+
+    BinarySearch(0, nums.size() - 1, nums, target, true);
+    BinarySearch(0, nums.size() - 1, nums, target, false);
+
+    if (minIndex == INT_MAX) minIndex = -1;
+    if (maxIndex == INT_MIN) maxIndex = -1;
+
+    return {minIndex, maxIndex};
+}
+
+int main() {
+    vector<int> nums = {5, 7, 7, 8, 8, 10};
+    int target = 8;
+
+    vector<int> result = SearchRange(nums, target);
+    cout << "[" << result[0] << ", " << result[1] << "]" << endl;
+
+    return 0;
+}
+
+
+
+
+c#:
+
 using System;
 using System.Collections.Generic;
 
