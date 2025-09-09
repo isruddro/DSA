8bcd08c83dc999255f2cf10782d11c723d86b0c6
Protap Ruddrodeb Sarker <93969345+ruddro-dev@users.noreply.github.com>
2025-09-09 09:50:10 +0600

    Update 17 Search a row and col wise sorted 2d matrix.cs

--- Aditya Verma/Binary Search/17 Search a row and col wise sorted 2d matrix.cs
+++ Aditya Verma/Binary Search/17 Search a row and col wise sorted 2d matrix.cs
@@ -1,3 +1,45 @@
+cpp:
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool SearchMatrix(const vector<vector<int>>& matrix, int target) {
+    int i = 0;
+    int j = matrix[0].size() - 1;
+
+    while (i < matrix.size() && j >= 0) {
+        if (matrix[i][j] == target)
+            return true;
+        else if (target < matrix[i][j])
+            j--;
+        else
+            i++;
+    }
+
+    return false;
+}
+
+int main() {
+    vector<vector<int>> matrix = {
+        {1, 4, 7, 11},
+        {2, 5, 8, 12},
+        {3, 6, 9, 16},
+        {10, 13, 14, 17}
+    };
+    int target = 5;
+
+    bool result = SearchMatrix(matrix, target);
+    cout << "Element " << target << " found: " << (result ? "true" : "false") << endl;
+
+    return 0;
+}
+
+
+
+c#:
+
+
 using System;
 using System.Collections.Generic;
 
