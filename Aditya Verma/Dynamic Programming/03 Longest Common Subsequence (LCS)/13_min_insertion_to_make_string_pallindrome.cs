76d87833741c956e855dc23cf83ac646042256f2
Protap Ruddrodeb Sarker <93969345+isruddro@users.noreply.github.com>
2025-09-10 00:32:01 +0600

    Update 13_min_insertion_to_make_string_pallindrome.cs

--- Aditya Verma/Dynamic Programming/03 Longest Common Subsequence (LCS)/13_min_insertion_to_make_string_pallindrome.cs
+++ Aditya Verma/Dynamic Programming/03 Longest Common Subsequence (LCS)/13_min_insertion_to_make_string_pallindrome.cs
@@ -1,3 +1,58 @@
+py:
+
+using System;
+using System.Linq;
+
+class Program
+{
+    // Function to compute the length of the Longest Common Subsequence (LCS)
+    static int LCS(string X, string Y, int n, int m)
+    {
+        int[,] dp = new int[n + 1, m + 1];
+
+        for (int i = 0; i <= n; i++)
+            for (int j = 0; j <= m; j++)
+                if (i == 0 || j == 0)
+                    dp[i, j] = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                if (X[i - 1] == Y[j - 1])
+                    dp[i, j] = 1 + dp[i - 1, j - 1];
+                else
+                    dp[i, j] = Math.Max(dp[i - 1, j], dp[i, j - 1]);
+            }
+        }
+
+        return dp[n, m];
+    }
+
+    // Function to compute the Longest Palindromic Subsequence (LPS)
+    static int LPS(string X, int n)
+    {
+        string rev_X = new string(X.Reverse().ToArray());
+        return LCS(X, rev_X, n, n);
+    }
+
+    // Function to compute the minimum insertions required to make the string a palindrome
+    static int MinInsertForPalindrome(string X, int n)
+    {
+        return n - LPS(X, n);
+    }
+
+    static void Main()
+    {
+        string X = Console.ReadLine();
+        int n = X.Length;
+
+        Console.WriteLine(MinInsertForPalindrome(X, n));
+    }
+}
+
+
+
 cpp:
 #include <bits/stdc++.h>
 using namespace std;
