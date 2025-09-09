29de7406907033b6eeabef1654610250f0902645
Protap Ruddrodeb Sarker <93969345+ruddro-dev@users.noreply.github.com>
2025-09-09 10:03:07 +0600

    Update 04_LCSubstring.cs

--- Aditya Verma/Dynamic Programming/03 Longest Common Subsequence (LCS)/04_LCSubstring.cs
+++ Aditya Verma/Dynamic Programming/03 Longest Common Subsequence (LCS)/04_LCSubstring.cs
@@ -1,3 +1,46 @@
+cpp:
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm> // for std::max
+
+using namespace std;
+
+int LCSubstr(const string &X, const string &Y, int n, int m) {
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+    int mx = 0; // maximum length of common substring
+
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= m; j++) {
+            if (i == 0 || j == 0)
+                dp[i][j] = 0;
+            else if (X[i - 1] == Y[j - 1])
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            else
+                dp[i][j] = 0;
+
+            mx = max(mx, dp[i][j]);
+        }
+    }
+
+    return mx;
+}
+
+int main() {
+    string X, Y;
+    getline(cin, X);
+    getline(cin, Y);
+
+    int result = LCSubstr(X, Y, X.length(), Y.length());
+    cout << result << endl;
+
+    return 0;
+}
+
+
+c#:
+
 using System;
 
 public class Solution
