04499ce4c6bb13e9f04c5332f76c1f635c0ebd0c
Protap Ruddrodeb Sarker <93969345+ruddro-dev@users.noreply.github.com>
2025-09-09 10:04:56 +0600

    Update 07_min_insertion_del__to_convert_a_to_b.cs

--- Aditya Verma/Dynamic Programming/03 Longest Common Subsequence (LCS)/07_min_insertion_del__to_convert_a_to_b.cs
+++ Aditya Verma/Dynamic Programming/03 Longest Common Subsequence (LCS)/07_min_insertion_del__to_convert_a_to_b.cs
@@ -1,3 +1,55 @@
+cpp:
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+// Function to calculate LCS length
+int LCS(const string &X, const string &Y, int n, int m) {
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            if (X[i - 1] == Y[j - 1])
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            else
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+
+    return dp[n][m];
+}
+
+// Function to calculate minimum insertions and deletions
+void MinInsertDel(const string &X, const string &Y) {
+    int n = X.length();
+    int m = Y.length();
+    int lcs_len = LCS(X, Y, n, m);
+
+    int deletions = n - lcs_len;  // Characters to delete from X
+    int insertions = m - lcs_len; // Characters to insert into X
+
+    cout << "Deletions: " << deletions << endl;
+    cout << "Insertions: " << insertions << endl;
+    cout << "Total Operations: " << (deletions + insertions) << endl;
+}
+
+int main() {
+    string X, Y;
+    getline(cin, X);
+    getline(cin, Y);
+
+    MinInsertDel(X, Y);
+
+    return 0;
+}
+
+
+
+
+c#:
+
 using System;
 
 public class Solution
