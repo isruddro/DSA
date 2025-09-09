83422286a6297a66cd8185b2da9435e7fa22559e
Protap Ruddrodeb Sarker <93969345+ruddro-dev@users.noreply.github.com>
2025-09-09 10:13:39 +0600

    Update 07_evaluate_expression_to_true_memoization_using_map.cs

--- Aditya Verma/Dynamic Programming/04 Matrix Chain Multiplication (MCM)/07_evaluate_expression_to_true_memoization_using_map.cs
+++ Aditya Verma/Dynamic Programming/04 Matrix Chain Multiplication (MCM)/07_evaluate_expression_to_true_memoization_using_map.cs
@@ -1,3 +1,65 @@
+cpp:
+#include <bits/stdc++.h>
+using namespace std;
+
+// Memoization map
+map<string, int> ump;
+
+int Solve(const string &X, int i, int j, bool isTrue) {
+    string key = to_string(i) + " " + to_string(j) + " " + (isTrue ? "T" : "F");
+
+    if (ump.find(key) != ump.end())
+        return ump[key];
+
+    // Base case
+    if (i >= j) {
+        ump[key] = (isTrue ? X[i] == 'T' : X[i] == 'F') ? 1 : 0;
+        return ump[key];
+    }
+
+    int ans = 0;
+
+    for (int k = i + 1; k < j; k += 2) {
+        int l_T = Solve(X, i, k - 1, true);
+        int l_F = Solve(X, i, k - 1, false);
+        int r_T = Solve(X, k + 1, j, true);
+        int r_F = Solve(X, k + 1, j, false);
+
+        if (X[k] == '|') {
+            if (isTrue)
+                ans += l_T * r_T + l_T * r_F + l_F * r_T;
+            else
+                ans += l_F * r_F;
+        } else if (X[k] == '&') {
+            if (isTrue)
+                ans += l_T * r_T;
+            else
+                ans += l_T * r_F + l_F * r_T + l_F * r_F;
+        } else if (X[k] == '^') {
+            if (isTrue)
+                ans += l_T * r_F + l_F * r_T;
+            else
+                ans += l_T * r_T + l_F * r_F;
+        }
+    }
+
+    return ump[key] = ans;
+}
+
+int main() {
+    string X;
+    cin >> X;
+
+    ump.clear();
+    cout << Solve(X, 0, X.size() - 1, true) << endl;
+
+    return 0;
+}
+
+
+
+c#:
+
 using System;
 using System.Collections.Generic;
 
