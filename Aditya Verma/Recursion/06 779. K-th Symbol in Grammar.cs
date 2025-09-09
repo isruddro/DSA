fec63c3585c3d30e66e368df234cf0dec478750b
Protap Ruddrodeb Sarker <93969345+ruddro-dev@users.noreply.github.com>
2025-09-09 10:33:23 +0600

    Update 06 779. K-th Symbol in Grammar.cs

--- Aditya Verma/Recursion/06 779. K-th Symbol in Grammar.cs
+++ Aditya Verma/Recursion/06 779. K-th Symbol in Grammar.cs
@@ -1,3 +1,38 @@
+cpp:
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int kthGrammar(int n, int k) {
+        // Base case: first row, first element is 0
+        if (n == 1 && k == 1)
+            return 0;
+
+        // Number of elements in the first half of the row
+        int mid = 1 << (n - 2);  // 2^(n-2)
+
+        // If k is in the first half, same as previous row
+        if (k <= mid)
+            return kthGrammar(n - 1, k);
+        else
+            // If k is in the second half, invert the value from previous row
+            return 1 - kthGrammar(n - 1, k - mid);
+    }
+};
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+    Solution solution;
+    cout << solution.kthGrammar(n, k) << endl;
+
+    return 0;
+}
+
+
+c#:
 public class Solution {
     public int KthGrammar(int n, int k) {
         // Base case: when n is 1 and k is 1, the value is 0
