d2eb20bbc72d00cc0025bfb9e7485cfe8c1befc4
Protap Ruddrodeb Sarker <93969345+ruddro-dev@users.noreply.github.com>
2025-09-09 10:21:02 +0600

    Update KClose-org.cs

--- Aditya Verma/Heaps/KClose-org.cs
+++ Aditya Verma/Heaps/KClose-org.cs
@@ -1,3 +1,57 @@
+cpp:
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Pair {
+    int first, second;
+    Pair(int f, int s) : first(f), second(s) {}
+};
+
+// Comparator for max-heap based on squared distance
+struct Compare {
+    bool operator()(const pair<int, Pair>& a, const pair<int, Pair>& b) {
+        return a.first < b.first; // max-heap: larger distance comes first
+    }
+};
+
+int main() {
+    vector<Pair> arr = {
+        Pair(1, 3),
+        Pair(-2, 2),
+        Pair(5, 8),
+        Pair(0, 3)
+    };
+    int k = 2;
+
+    priority_queue<pair<int, Pair>, vector<pair<int, Pair>>, Compare> maxh;
+
+    for (auto &point : arr) {
+        int dis = point.first * point.first + point.second * point.second;
+        maxh.push({dis, point});
+
+        if ((int)maxh.size() > k)
+            maxh.pop();
+    }
+
+    vector<Pair> result;
+    while (!maxh.empty()) {
+        result.push_back(maxh.top().second);
+        maxh.pop();
+    }
+
+    // Optional: reverse to get points from closest to farthest
+    reverse(result.begin(), result.end());
+
+    for (auto &p : result) {
+        cout << p.first << ", " << p.second << "; ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
+
+c#:
 using System;
 using System.Collections.Generic;
 
