3cde126e431692cd3060017f97708b9779470af9
Protap Ruddrodeb Sarker <93969345+ruddro-dev@users.noreply.github.com>
2025-09-09 10:11:40 +0600

    Update 03_pallindrome_partitioning_recursive.cs

--- Aditya Verma/Dynamic Programming/04 Matrix Chain Multiplication (MCM)/03_pallindrome_partitioning_recursive.cs
+++ Aditya Verma/Dynamic Programming/04 Matrix Chain Multiplication (MCM)/03_pallindrome_partitioning_recursive.cs
@@ -1,3 +1,49 @@
+cpp:
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to check if a substring is palindrome
+bool IsPalindrome(const string &X, int i, int j) {
+    while (i <= j) {
+        if (X[i] != X[j])
+            return false;
+        i++;
+        j--;
+    }
+    return true;
+}
+
+// Recursive function to compute minimum cuts
+int Solve(const string &X, int i, int j) {
+    // Base case: already palindrome or length <= 1
+    if (i >= j || IsPalindrome(X, i, j))
+        return 0;
+
+    int ans = INT_MAX;
+
+    // Try all possible partitions
+    for (int k = i; k <= j - 1; k++) {
+        int tempAns = Solve(X, i, k) + Solve(X, k + 1, j) + 1;
+        ans = min(ans, tempAns);
+    }
+
+    return ans;
+}
+
+int main() {
+    string X;
+    cin >> X;
+
+    cout << Solve(X, 0, X.length() - 1) << endl;
+
+    return 0;
+}
+
+
+
+
+c#:
+
 using System;
 
 class PalindromePartition
