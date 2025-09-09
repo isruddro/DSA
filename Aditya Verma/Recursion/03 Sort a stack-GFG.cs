3c0f8aecf5c87a79c0ad29615351dcdfa5900e9b
Protap Ruddrodeb Sarker <93969345+ruddro-dev@users.noreply.github.com>
2025-09-09 10:31:57 +0600

    Update 03 Sort a stack-GFG.cs

--- Aditya Verma/Recursion/03 Sort a stack-GFG.cs
+++ Aditya Verma/Recursion/03 Sort a stack-GFG.cs
@@ -1,3 +1,80 @@
+cpp:
+#include <bits/stdc++.h>
+using namespace std;
+
+class SortedStack {
+private:
+    stack<int> s;
+
+    // Helper function to insert element into sorted stack
+    void getStack(stack<int>& st, int to) {
+        if (st.empty() || to >= st.top()) {
+            st.push(to);
+            return;
+        }
+
+        int t = st.top();
+        st.pop();
+        getStack(st, to);
+        st.push(t);
+    }
+
+    // Recursive sort function
+    void sortStack() {
+        if (s.empty())
+            return;
+
+        int to = s.top();
+        s.pop();
+        sortStack();
+        getStack(s, to);
+    }
+
+public:
+    // Push element into stack
+    void push(int value) {
+        s.push(value);
+    }
+
+    // Sort the stack
+    void sort() {
+        sortStack();
+    }
+
+    // Print the stack elements (top to bottom)
+    void printStack() {
+        stack<int> temp = s; // Copy to preserve original stack
+        vector<int> v;
+        while (!temp.empty()) {
+            v.push_back(temp.top());
+            temp.pop();
+        }
+        reverse(v.begin(), v.end()); // To print from bottom to top
+        for (int num : v)
+            cout << num << " ";
+        cout << endl;
+    }
+};
+
+int main() {
+    SortedStack sortedStack;
+
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int val;
+        cin >> val;
+        sortedStack.push(val);
+    }
+
+    sortedStack.sort();
+    sortedStack.printStack();
+
+    return 0;
+}
+
+
+c#:
 using System;
 using System.Collections.Generic;
 
