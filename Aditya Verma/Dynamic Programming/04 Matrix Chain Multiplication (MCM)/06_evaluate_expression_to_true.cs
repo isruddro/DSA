b79863a03af04b2ef9f637ef64fbe8f79715eafe
Protap Ruddrodeb Sarker <93969345+ruddro-dev@users.noreply.github.com>
2025-09-09 10:13:05 +0600

    Update 06_evaluate_expression_to_true.cs

--- Aditya Verma/Dynamic Programming/04 Matrix Chain Multiplication (MCM)/06_evaluate_expression_to_true.cs
+++ Aditya Verma/Dynamic Programming/04 Matrix Chain Multiplication (MCM)/06_evaluate_expression_to_true.cs
@@ -1,3 +1,58 @@
+cpp:
+#include <bits/stdc++.h>
+using namespace std;
+
+// Recursive function to count the number of ways
+int Solve(const string &X, int i, int j, bool isTrue) {
+    if (i >= j) {
+        if (isTrue)
+            return X[i] == 'T' ? 1 : 0;
+        else
+            return X[i] == 'F' ? 1 : 0;
+    }
+
+    int ans = 0;
+
+    // Loop through all operators in the expression
+    for (int k = i + 1; k < j; k += 2) {
+        int l_T = Solve(X, i, k - 1, true);
+        int l_F = Solve(X, i, k - 1, false);
+        int r_T = Solve(X, k + 1, j, true);
+        int r_F = Solve(X, k + 1, j, false);
+
+        if (X[k] == '|') {
+            if (isTrue)
+                ans += l_T * r_T + l_T * r_F + l_F * r_T;
+            else
+                ans += l_F * r_F;
+        } else if (X[k] == '&') {
+            if (isTrue)
+                ans += l_T * r_T;
+            else
+                ans += l_T * r_F + l_F * r_T + l_F * r_F;
+        } else if (X[k] == '^') {
+            if (isTrue)
+                ans += l_T * r_F + l_F * r_T;
+            else
+                ans += l_T * r_T + l_F * r_F;
+        }
+    }
+
+    return ans;
+}
+
+int main() {
+    string X;
+    cin >> X;
+
+    cout << Solve(X, 0, X.size() - 1, true) << endl; // Number of ways to make the expression TRUE
+    return 0;
+}
+
+
+
+c#:
+
 using System;
 
 class BooleanParenthesization
