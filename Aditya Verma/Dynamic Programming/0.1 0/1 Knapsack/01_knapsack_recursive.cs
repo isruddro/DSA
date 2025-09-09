7c0100ead0eb38988325ae3b0627148d849936ef
Protap Ruddrodeb Sarker <93969345+ruddro-dev@users.noreply.github.com>
2025-09-09 09:51:27 +0600

    Update 01_knapsack_recursive.cs

--- Aditya Verma/Dynamic Programming/0.1 0/1 Knapsack/01_knapsack_recursive.cs
+++ Aditya Verma/Dynamic Programming/0.1 0/1 Knapsack/01_knapsack_recursive.cs
@@ -1,3 +1,42 @@
+cpp:
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// Recursive 0/1 Knapsack function
+int Knapsack(const vector<int>& wt, const vector<int>& val, int W, int n) {
+    // Base case
+    if (n == 0 || W == 0)
+        return 0;
+
+    // If weight of nth item is less than or equal to capacity
+    if (wt[n - 1] <= W) {
+        return max(val[n - 1] + Knapsack(wt, val, W - wt[n - 1], n - 1),
+                   Knapsack(wt, val, W, n - 1));
+    }
+    else { // If weight of nth item is more than capacity
+        return Knapsack(wt, val, W, n - 1);
+    }
+}
+
+int main() {
+    vector<int> wt = {1, 3, 4, 5};  // Weights
+    vector<int> val = {1, 4, 5, 7}; // Values
+    int W = 7;                       // Knapsack capacity
+
+    int n = wt.size(); // Number of items
+
+    cout << Knapsack(wt, val, W, n) << endl;
+
+    return 0;
+}
+
+
+
+
+c#:
+
 using System;
 class Program
 {
