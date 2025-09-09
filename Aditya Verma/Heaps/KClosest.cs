5f5528290e77a228150e68509e11dd90cc47f21d
Protap Ruddrodeb Sarker <93969345+ruddro-dev@users.noreply.github.com>
2025-09-09 10:21:36 +0600

    Update KClosest.cs

--- Aditya Verma/Heaps/KClosest.cs
+++ Aditya Verma/Heaps/KClosest.cs
@@ -1,3 +1,48 @@
+cpp:
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> arr = {5, 6, 7, 8, 9};
+    int k;
+    int m = 7; // target
+    int n = arr.size();
+
+    cout << "Enter k: ";
+    cin >> k;
+
+    // Max-heap: pair<abs_diff, value>, larger diff on top
+    priority_queue<pair<int, int>> maxhp;
+
+    for (int num : arr) {
+        int diff = abs(num - m);
+        maxhp.push({diff, num});
+
+        if ((int)maxhp.size() > k)
+            maxhp.pop();
+    }
+
+    cout << "The k closest numbers are: ";
+    vector<int> result;
+
+    while (!maxhp.empty()) {
+        result.push_back(maxhp.top().second);
+        maxhp.pop();
+    }
+
+    // Optional: sort to have them in order closest to farthest
+    sort(result.begin(), result.end());
+    for (int num : result)
+        cout << num << " ";
+    cout << endl;
+
+    return 0;
+}
+
+
+
+c#:
+
 using System;
 using System.Collections.Generic;
 
