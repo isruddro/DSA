db334cc4abee79ba69719c1e65c05bb2cbb1743a
Protap Ruddrodeb Sarker <93969345+ruddro-dev@users.noreply.github.com>
2025-09-09 09:40:20 +0600

    Update 02 Binary Search Iter.cs

--- Aditya Verma/Binary Search/02 Binary Search Iter.cs
+++ Aditya Verma/Binary Search/02 Binary Search Iter.cs
@@ -1,3 +1,42 @@
+cpp:
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> list = {1,2,3,4,5,6,7,8,9,10};
+    int toFind = 10;
+    int ans = -1;
+
+    int start = 0, end = list.size() - 1;
+
+    while (start <= end) {
+        int mid = (start + end) / 2;
+        if (list[mid] == toFind) {
+            ans = mid;
+            break;
+        } else if (list[mid] > toFind) {
+            end = mid - 1;
+        } else {
+            start = mid + 1;
+        }
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+c#:
+
 using System;
 using System.Collections.Generic;
 
