700b40923f4edd3a40a84d535eb5dbf1bae20cea
Protap Ruddrodeb Sarker <93969345+isruddro@users.noreply.github.com>
2025-09-14 16:07:46 +0600

    Update 05_equal_sum_partition.cs

--- Aditya Verma/Dynamic Programming/0.1 0/1 Knapsack/05_equal_sum_partition.cs
+++ Aditya Verma/Dynamic Programming/0.1 0/1 Knapsack/05_equal_sum_partition.cs
@@ -1,5 +1,9 @@
 https://leetcode.com/problems/partition-equal-subset-sum/description/
 
+
+#same as subset sum but we check if the sum is possible in a funcion. If half of total sum is odd
+then partition is not possible.
+
 py:
 
 from typing import List
