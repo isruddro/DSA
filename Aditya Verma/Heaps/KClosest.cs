7488fcfdfbcd0b92f6d91f278f7bc82b21a73290
Protap Ruddrodeb Sarker <93969345+isruddro@users.noreply.github.com>
2025-09-16 17:29:28 +0600

    Update KClosest.cs

--- Aditya Verma/Heaps/KClosest.cs
+++ Aditya Verma/Heaps/KClosest.cs
@@ -4,7 +4,7 @@ https://www.geeksforgeeks.org/problems/k-closest-elements3619/0
         5 6 6 8 9
 abs (-) 7 7 7 7 7
     (=) 2 1 0 1 2
-        finally on the basis of the diff we get: 7 6 8 are k(3) closest numbers. (here x is 7)
+        finally on the basis of the diff we get: 7 6 8 are k(3) closest numbers. (here m is 7 = target number)
 
 * Closest means smallest. So we keeping smallest numbers, that is why we deleting maximum numbers. (using maxheap)
     
