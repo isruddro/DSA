0dbd5cb6cc30b258b21b340202b86ca5cfce3118
Protap Ruddrodeb Sarker <93969345+isruddro@users.noreply.github.com>
2025-09-25 10:46:14 +0600

    Update 11_longest_repeating_subseq.cs

--- Aditya Verma/Dynamic Programming/03 Longest Common Subsequence (LCS)/11_longest_repeating_subseq.cs
+++ Aditya Verma/Dynamic Programming/03 Longest Common Subsequence (LCS)/11_longest_repeating_subseq.cs
@@ -11,24 +11,26 @@ https://www.geeksforgeeks.org/problems/longest-repeating-subsequence2004/1
     * Rest is just LCS as both has same seq and it also repeats!
 
 py:
+O(n²) time, O(n²) space
 
-def LRS(X):
-    n = len(X)
-    dp = [[0] * (n + 1) for _ in range(n + 1)]
+#User function Template for python3
+class Solution:
+    def LongestRepeatingSubsequence(self, s):
+        X = s
+        n = len(X)
+        dp = [[0] * (n + 1) for _ in range(n + 1)]
 
-    for i in range(1, n + 1):
-        for j in range(1, n + 1):
-            # Characters match and indices are different
-            if X[i - 1] == X[j - 1] and i != j:
-                dp[i][j] = 1 + dp[i - 1][j - 1]
-            else:
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        # Fill DP table
+        for i in range(1, n + 1):
+            for j in range(1, n + 1):
+                # Characters match and indices are different
+                if X[i - 1] == X[j - 1] and i != j:
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
 
-    return dp[n][n]
+        return dp[n][n]
 
-# Example usage
-X = input()
-print(LRS(X))
 
 
 
