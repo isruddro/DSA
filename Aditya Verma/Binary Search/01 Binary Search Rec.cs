48731dc9b7134b8600ee494bd3fa69cf20351284
Protap Ruddrodeb Sarker <93969345+isruddro@users.noreply.github.com>
2025-09-26 17:40:02 +0600

    Update 01 Binary Search Rec.cs

--- Aditya Verma/Binary Search/01 Binary Search Rec.cs
+++ Aditya Verma/Binary Search/01 Binary Search Rec.cs
@@ -1,9 +1,36 @@
 https://leetcode.com/problems/binary-search/description/
 
-py:
+
 Time: O(log n)
 
 Space: O(1)
+cpp:
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        return binarySearch(nums, target, 0, nums.size() - 1);
+    }
+
+private:
+    int binarySearch(vector<int>& nums, int target, int start, int end) {
+        if (start > end)
+            return -1;
+
+        int mid = (start + end) / 2;
+
+        if (nums[mid] == target)
+            return mid;
+        else if (nums[mid] > target)
+            return binarySearch(nums, target, start, mid - 1);
+        else
+            return binarySearch(nums, target, mid + 1, end);
+    }
+};
+
+py3:
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         def binarySearch(start: int, end: int) -> int:
